cc16a8c456fb90ebe2a953fc53af9aebb60f276f
vizvarun <varunvishal10@gmail.com>
2020-07-06 05:55:45 +0530

    Updates

---
+++ precache-manifest.e641790726cc89f8148ecaaae916f179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b22cbb75afed486f594c528999e2e1f",
+    "url": "/search-cards/index.html"
+  },
+  {
+    "revision": "dd80d05db99dcbc23000",
+    "url": "/search-cards/static/css/main.ed46f033.chunk.css"
+  },
+  {
+    "revision": "1917165c8983ba8f57c5",
+    "url": "/search-cards/static/js/2.2a05d065.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-cards/static/js/2.2a05d065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd80d05db99dcbc23000",
+    "url": "/search-cards/static/js/main.d868c074.chunk.js"
+  },
+  {
+    "revision": "6fb8ec5e1d0dc11198fb",
+    "url": "/search-cards/static/js/runtime-main.4754c5b7.js"
+  }
+]);
